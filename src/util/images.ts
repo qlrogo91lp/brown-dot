b114690aa68dff6704478225aa8e633211acc512
yjkim <qlrogo91lp@gmail.com>
2025-05-16 13:59:57 +0900

    🍱 이미지 파일 추가 및 favicon 파일 추가

---
+++ src/util/images.ts
@@ -0,0 +1,66 @@
+import type { ReactImageGalleryItem } from 'react-image-gallery';
+
+export const exteriors: ReactImageGalleryItem[] = [
+  { original: '/images/exterior/HCJ.jpg' },
+  { original: '/images/exterior/HCJ-2.jpg' },
+  { original: '/images/nightview/nightview.jpg' },
+];
+
+export const deluexA: ReactImageGalleryItem[] = [
+  { original: '/images/room/deluxe_A/HCJ.jpg' },
+  { original: '/images/room/deluxe_A/HCJ-2.jpg' },
+  { original: '/images/room/deluxe_A/HCJ-3.jpg' },
+  { original: '/images/room/deluxe_A/HCJ-4.jpg' },
+  { original: '/images/room/deluxe_A/HCJ-5.jpg' },
+  { original: '/images/room/deluxe_A/HCJ-6.jpg' },
+  { original: '/images/room/deluxe_A/HCJ-7.jpg' },
+];
+
+export const deluexB: ReactImageGalleryItem[] = [
+  { original: '/images/room/deluxe_B/HCJ.jpg' },
+  { original: '/images/room/deluxe_B/HCJ-2.jpg' },
+  { original: '/images/room/deluxe_B/HCJ-3.jpg' },
+  { original: '/images/room/deluxe_B/HCJ-4.jpg' },
+  { original: '/images/room/deluxe_B/HCJ-5.jpg' },
+  { original: '/images/room/deluxe_B/HCJ-6.jpg' },
+  { original: '/images/room/deluxe_B/HCJ-7.jpg' },
+];
+
+export const premium: ReactImageGalleryItem[] = [
+  { original: '/images/room/premium/HCJ.jpg' },
+  { original: '/images/room/premium/HCJ-2.jpg' },
+  { original: '/images/room/premium/HCJ-3.jpg' },
+  { original: '/images/room/premium/HCJ-4.jpg' },
+  { original: '/images/room/premium/HCJ-5.jpg' },
+  { original: '/images/room/premium/HCJ-6.jpg' },
+];
+
+export const standard: ReactImageGalleryItem[] = [
+  { original: '/images/room/standard/HCJ.jpg' },
+  { original: '/images/room/standard/HCJ-2.jpg' },
+  { original: '/images/room/standard/HCJ-3.jpg' },
+  { original: '/images/room/standard/HCJ-4.jpg' },
+  { original: '/images/room/standard/HCJ-5.jpg' },
+  { original: '/images/room/standard/HCJ-6.jpg' },
+  { original: '/images/room/standard/HCJ-7.jpg' },
+];
+
+export const suite: ReactImageGalleryItem[] = [
+  { original: '/images/room/suite/HCJ.jpg' },
+  { original: '/images/room/suite/HCJ-2.jpg' },
+  { original: '/images/room/suite/HCJ-3.jpg' },
+  { original: '/images/room/suite/HCJ-4.jpg' },
+  { original: '/images/room/suite/HCJ-5.jpg' },
+  { original: '/images/room/suite/HCJ-6.jpg' },
+  { original: '/images/room/suite/HCJ-7.jpg' },
+];
+
+export const twin: ReactImageGalleryItem[] = [
+  { original: '/images/room/twin/HCJ.jpg' },
+  { original: '/images/room/twin/HCJ-2.jpg' },
+  { original: '/images/room/twin/HCJ-3.jpg' },
+  { original: '/images/room/twin/HCJ-4.jpg' },
+  { original: '/images/room/twin/HCJ-5.jpg' },
+  { original: '/images/room/twin/HCJ-6.jpg' },
+  { original: '/images/room/twin/HCJ-7.jpg' },
+];
