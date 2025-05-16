58af80bdbb7047c1dbe371890d54018b9d640f8a
yjkim <qlrogo91lp@gmail.com>
2025-05-16 17:34:57 +0900

    🩹 편의시설 내용 수정

--- src/components/AmenitiesInfo.tsx
+++ src/components/AmenitiesInfo.tsx
@@ -1,12 +1,11 @@
 export default function AmenitiesInfo() {
 	return (
 		<section className="p-6">
-			<h2 className="text-xl font-bold mb-2">편의 시설</h2>
-			<ul className="list-disc pl-5 text-gray-700 space-y-1">
+			<h2 className="mb-2 text-xl font-bold">편의 시설</h2>
+			<ul className="pl-5 space-y-1 text-gray-700 list-disc">
 				<li>무료 Wi-Fi</li>
-				<li>조식 서비스</li>
-				<li>셀프 체크인</li>
-				<li>공용 주방</li>
+				<li>조식 서비스 (순쌀빵 등)</li>
+				<li>OTT</li>
 			</ul>
 		</section>
 	);
